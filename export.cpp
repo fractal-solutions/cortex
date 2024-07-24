2159646fe3bd7d0fb7caa45c4883bcda24f79d89
fractal-solutions <fractal_solutions@protonmail.com>
2024-07-24 16:36:01 +0300

    modified:   dqn.dll 	modified:   export.cpp 	modified:   export.h

--- export.cpp
+++ export.cpp
@@ -4,47 +4,47 @@
 
 extern "C" {
 
-DQN* __stdcall CreateDQN(int stateSize, int actionSize, int* hiddenLayers, int hiddenLayersSize) {
+DQN* DLL_CALL CreateDQN(int stateSize, int actionSize, int* hiddenLayers, int hiddenLayersSize) {
     std::vector<int> hiddenLayersVec(hiddenLayers, hiddenLayers + hiddenLayersSize);
     return new DQN(stateSize, actionSize, hiddenLayersVec);
 }
 
-void __stdcall DestroyDQN(DQN* dqn) {
+void DLL_CALL DestroyDQN(DQN* dqn) {
     delete dqn;
 }
 
-int __stdcall SelectAction(DQN* dqn, double* state, int stateSize, double epsilon) {
+int DLL_CALL SelectAction(DQN* dqn, double* state, int stateSize, double epsilon) {
     std::vector<double> stateVec(state, state + stateSize);
     return dqn->SelectAction(stateVec, epsilon);
 }
 
-void __stdcall Train(DQN* dqn, double* state, int stateSize, int action, double reward, double* nextState, int nextStateSize, double gamma, double epsilonDecay) {
+void DLL_CALL Train(DQN* dqn, double* state, int stateSize, int action, double reward, double* nextState, int nextStateSize, double gamma, double epsilonDecay) {
     std::vector<double> stateVec(state, state + stateSize);
     std::vector<double> nextStateVec(nextState, nextState + nextStateSize);
     dqn->Train(stateVec, action, reward, nextStateVec, gamma, epsilonDecay);
 }
 
-void __stdcall UpdateTargetNetwork(DQN* dqn) {
+void DLL_CALL UpdateTargetNetwork(DQN* dqn) {
     dqn->UpdateTargetNetwork();
 }
 
-double __stdcall GetGamma(DQN* dqn) {
+double DLL_CALL GetGamma(DQN* dqn) {
     return dqn->GetGamma();
 }
 
-double __stdcall GetEpsilon(DQN* dqn) {
+double DLL_CALL GetEpsilon(DQN* dqn) {
     return dqn->GetEpsilon();
 }
 
-double __stdcall GetEpsilonDecay(DQN* dqn) {
+double DLL_CALL GetEpsilonDecay(DQN* dqn) {
     return dqn->GetEpsilonDecay();
 }
 
-int __stdcall SaveModel(DQN* dqn, const char* filepath) {
+int DLL_CALL SaveModel(DQN* dqn, const char* filepath) {
     dqn->SaveModel(filepath);
 }
 
-int __stdcall LoadModel(DQN* dqn, const char* filepath) {
+int DLL_CALL LoadModel(DQN* dqn, const char* filepath) {
     dqn->LoadModel(filepath);
 }
 
--- export.h
+++ export.h
@@ -3,19 +3,28 @@
 
 #include "DQN.h"
 
+// Define cross-platform export macros
+#if defined(_WIN32) || defined(_WIN64)
+    #define DLL_EXPORT __declspec(dllexport)
+    #define DLL_CALL __stdcall
+#else
+    #define DLL_EXPORT __attribute__((visibility("default")))
+    #define DLL_CALL
+#endif
+
 // Export functions for the DLL
 extern "C" {
 
-__declspec(dllexport) DQN* __stdcall CreateDQN(int stateSize, int actionSize, int* hiddenLayers, int hiddenLayersSize);
-__declspec(dllexport) void __stdcall DestroyDQN(DQN* dqn);
-__declspec(dllexport) int __stdcall SelectAction(DQN* dqn, double* state, int stateSize, double epsilon);
-__declspec(dllexport) void __stdcall Train(DQN* dqn, double* state, int stateSize, int action, double reward, double* nextState, int nextStateSize, double gamma, double epsilonDecay);
-__declspec(dllexport) void __stdcall UpdateTargetNetwork(DQN* dqn);
-__declspec(dllexport) double __stdcall GetGamma(DQN* dqn);
-__declspec(dllexport) double __stdcall GetEpsilon(DQN* dqn);
-__declspec(dllexport) double __stdcall GetEpsilonDecay(DQN* dqn);
-__declspec(dllexport) int __stdcall SaveModel(DQN* dqn, const char* filepath);
-__declspec(dllexport) int __stdcall LoadModel(DQN* dqn, const char* filepath);
+DLL_EXPORT DQN* DLL_CALL CreateDQN(int stateSize, int actionSize, int* hiddenLayers, int hiddenLayersSize);
+DLL_EXPORT void DLL_CALL DestroyDQN(DQN* dqn);
+DLL_EXPORT int DLL_CALL SelectAction(DQN* dqn, double* state, int stateSize, double epsilon);
+DLL_EXPORT void DLL_CALL Train(DQN* dqn, double* state, int stateSize, int action, double reward, double* nextState, int nextStateSize, double gamma, double epsilonDecay);
+DLL_EXPORT void DLL_CALL UpdateTargetNetwork(DQN* dqn);
+DLL_EXPORT double DLL_CALL GetGamma(DQN* dqn);
+DLL_EXPORT double DLL_CALL GetEpsilon(DQN* dqn);
+DLL_EXPORT double DLL_CALL GetEpsilonDecay(DQN* dqn);
+DLL_EXPORT int DLL_CALL SaveModel(DQN* dqn, const char* filepath);
+DLL_EXPORT int DLL_CALL LoadModel(DQN* dqn, const char* filepath);
 
 }
 
